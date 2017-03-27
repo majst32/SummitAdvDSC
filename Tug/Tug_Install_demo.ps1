e97b045397fa62d25e23d03980222d89db131eb0
Melissa Januszko <majst32@yahoo.com>
2017-03-27 13:57:16 -0700

    Add Tug Install demo

---
+++ Tug/Tug_Install_demo.ps1
@@ -0,0 +1,17 @@
+﻿Register-PSRepository -Name tug-pre -SourceLocation https://www.myget.org/F/tug/api/v2
+Find-Module -Repository tug-pre
+Install-Module -Repository tug-pre Tug.Server-ps5
+
+
+#Upgrade tug install
+Remove-Module tug.Server-ps5 -force
+Uninstall-Module Tug.Server-ps5 -force
+Install-Module -Repository tug-pre Tug.Server-ps5
+
+Import-Module Tug.Server-ps5
+Install-TugServer #-overwrite
+
+cd 'C:\Program Files\Tug\Server'
+.\tug-server.cmd
+
+New-NetFirewallRule -name "Tug Inbound" -DisplayName "Tug Inbound" -Enabled true -Direction Inbound -Protocol TCP -LocalPort 5000
