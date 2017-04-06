d9617ec9bf6e0d48646212068afe5bb5792a37ab
Melissa Januszko <majst32@yahoo.com>
2017-04-06 12:10:28 -0700

    Added dscea demo

---
+++ DSCEA/DSCEADemo.ps1
@@ -0,0 +1,9 @@
+﻿Start-DSCEAscan -MofFile C:\DSC\Configs\TgtPull.mof -ComputerName CLI1,TgtPull
+Get-DSCEAreport -Overall
+explorer .\OverallComplianceReport.html
+
+Start-DSCEAscan -MofFile C:\dsc\Configs\ENTSub.mof -ComputerName EntSub
+Get-DSCEAreport -ComputerName entsub
+explorer .\ComputerComplianceReport-entsub.html
+
+explorer https://microsoft.github.io/DSCEA/
---
+++ DSCEA/FileTest.ps1
@@ -0,0 +1,17 @@
+﻿Configuration FileTest {
+    
+    param (
+        [string[]]$ComputerName
+        )
+
+    Node $ComputerName {
+        
+        file TempDir {
+            Ensure = 'present'
+            Type = 'Directory'
+            DestinationPath = "C:\temp"
+            }
+        }
+    }
+FileTest -ComputerName Cli1,TgtPull -outputPath C:\DSC\Configs
+        
